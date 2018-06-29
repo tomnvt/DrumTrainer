0a523e2371045890ea177c61f61e15cd6304086c
“tomnvt” <tom.novota@gmail.com>
2018-06-29 23:48:56 +0200

    Add tags to buttons and connect them with IBAction

--- TrainYaDrums/ViewController.swift
+++ TrainYaDrums/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
 }
 
