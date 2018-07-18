64f2d84a54666810401a30bd531c5b7cbf4e1da2
tomnvt <tnovotny@simpleway.cz>
2018-07-18 23:07:42 +0200

    Add UIView extension for setting view border width and border color

---
+++ TrainYaDrums/Extension/UIView+Border.swift
@@ -0,0 +1,38 @@
+//
+//  UIView+Border.swift
+//  DrumTrainer
+//
+//  Created by NVT on 17.07.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
