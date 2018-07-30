291ea3ce148c2d7526295887bebb09b2abdbb537
tomnvt <tnovotny@simpleway.cz>
2018-07-30 22:36:07 +0200

    Class for synchronizing subclasses with global clock

---
+++ TrainYaDrums/Synchronizable.swift
@@ -0,0 +1,39 @@
+//
+//  Synchronizable.swift
+//  DrumTrainer
+//
+//  Created by NVT on 29.07.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Foundation
+
+class Synchronizable {
+    
+    var beatIndices = [0, 1, 2, 3]
+    var currentBeatIndex = 0
+    
+    let globalClockBeat = Notification.Name(rawValue: "globalClockBeat")
+    let globalClockBar = Notification.Name(rawValue: "globalClockBar")
+
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(playSynchronized), name: globalClockBeat, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(setBeatIndexToZero), name: globalClockBar, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(synchronizeWithGlobalClock), name: globalClockBeat, object: nil)
+    }
+    
+    @objc func playSynchronized() {}
+    
+    @objc func setBeatIndexToZero() {
+        currentBeatIndex = 0
+    }
+    
+    @objc func synchronizeWithGlobalClock() {
+        if currentBeatIndex < 3 {
+            currentBeatIndex += 1
+        } else {
+            currentBeatIndex = 0
+        }
+    }
+    
+}
