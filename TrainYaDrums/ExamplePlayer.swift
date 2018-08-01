8affbcf05321a37648f17cb5a770deb54b063c6b
tomnvt <tnovotny@simpleway.cz>
2018-08-01 19:21:30 +0200

    Inherit from Synchronizable class

--- TrainYaDrums/ExamplePlayer.swift
+++ TrainYaDrums/ExamplePlayer.swift
@@ -12,40 +12,17 @@ protocol ExamplePlayerDelegate {
     func playDrum(beatpadNumber: [Int])
 }
 
-class ExamplePlayer {
-
-    var timer = Timer()
-    var examplePartIndex = 0
+class ExamplePlayer : Synchronizable {
+    
+    var drumExampleIsPlaying = false
     let exampleBeatSequence = [[0], [2], [0, 1], [2], [0], [2], [0, 1], [2]]
     var delegate : ExamplePlayerDelegate?
-    var beatIndices = [0, 1, 2, 3]
-    var currentBeatIndex = 0
-    var drumExampleIsPlaying = false
 
-    init() {
-        NotificationCenter.default.addObserver(self, selector: #selector(setExamplePartIndexZero), name: Notification.Name(rawValue: "globalClockBar"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(playExamplePart), name: Notification.Name(rawValue: "globalClockBeat"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(synchronizeWithGlobalClock), name: Notification.Name(rawValue: "globalClockBeat"), object: nil)
-    }
-
-    @objc func setExamplePartIndexZero() {
-        examplePartIndex = 0
-    }
-    
-    @objc func playExamplePart() {
+    override func playSynchronized() {
         guard drumExampleIsPlaying else {
             return
         }
         delegate?.playDrum(beatpadNumber: exampleBeatSequence[currentBeatIndex])
-    }
-    
-    @objc func synchronizeWithGlobalClock() {
-        print(beatIndices[currentBeatIndex])
-        if currentBeatIndex < 3 {
-            currentBeatIndex += 1
-        } else {
-            currentBeatIndex = 0
-        }
     }
     
 }
