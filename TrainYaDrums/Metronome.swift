1fafa4c4104574535b19845dc0aa6b311218006e
tomnvt <tnovotny@simpleway.cz>
2018-08-12 11:00:26 +0200

    Change metronome audio player from AVAudioPlayer to AKAudioPllayer

--- TrainYaDrums/Metronome.swift
+++ TrainYaDrums/Metronome.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import AVFoundation
+import AudioKit
 
 protocol MetronomeButtonFlashDelegate: AnyObject {
     func metronomeButtonFlash()
@@ -19,20 +20,59 @@ class Metronome: Synchronizable {
     var playerVolume: Float = 0.8
     weak var delegate: MetronomeButtonFlashDelegate?
     var player: AVAudioPlayer?
+    var metronomeSounds = ["metronomeSound1.wav",
+                           "metronomeSound2.wav"]
+    var firstMetronomeSoundAudioPlayer: AKAudioPlayer?
+    var secondMetronomeSoundAudioPlayer: AKAudioPlayer?
+    var audioMixer = AKMixer()
+
+    override init() {
+        super.init()
+        loadMetronomeSounds()
+        connectMetronomeSoundPlayersToMixer()
+        startAudioKit()
+    }
+
+    
+    func loadMetronomeSounds() {
+        do {
+            let firstMetronomeSoundAudioFile = try AKAudioFile(readFileName: metronomeSounds[0])
+            let secondMetronomeSoundAudioFile = try AKAudioFile(readFileName: metronomeSounds[1])
+            firstMetronomeSoundAudioPlayer = try AKAudioPlayer(file: firstMetronomeSoundAudioFile)
+            secondMetronomeSoundAudioPlayer = try AKAudioPlayer(file: secondMetronomeSoundAudioFile)
+        } catch {
+            print("ERROR: Metronome sound file is missing")
+        }
+    }
+
+    fileprivate func startAudioKit() {
+        AudioKit.output = audioMixer
+        do {
+            try AudioKit.start()
+        } catch {
+            print("ERROR: AudioKit failed to start")
+        }
+    }
+
+    fileprivate func connectMetronomeSoundPlayersToMixer() {
+        audioMixer.connect(input: firstMetronomeSoundAudioPlayer)
+        audioMixer.connect(input: secondMetronomeSoundAudioPlayer)
+    }
 
     override func playSynchronized() {
         guard metronomeIsRunning else {
             return
         }
         if currentBeatIndex == 0 {
-            playMetronome(sound: "metronomeSound1")
+            firstMetronomeSoundAudioPlayer?.play()
         } else {
-            playMetronome(sound: "metronomeSound2")
+            secondMetronomeSoundAudioPlayer?.play()
         }
         delegate?.metronomeButtonFlash()
     }
 
     //- MARK: Add independent Player class
+    //- MARK: Change AVAudioPlayer to AudioKit routing
     @objc func playMetronome(sound: String) {
         guard let url = Bundle.main.url(forResource: sound, withExtension: "wav") else { return }
 
