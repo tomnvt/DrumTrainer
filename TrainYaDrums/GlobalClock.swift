3ee0b4f66c852613a3af12804e0e8b97e12c78f9
tomnvt <tnovotny@simpleway.cz>
2018-08-13 22:57:18 +0200

    Adjust global clock to send eighth note interval notifications

--- TrainYaDrums/GlobalClock.swift
+++ TrainYaDrums/GlobalClock.swift
@@ -11,24 +11,42 @@ import Foundation
 class GlobalClock {
 
     var timer: Timer = Timer()
+    var halfBeatTimer: Timer = Timer()
+    var quaterBeatTimer: Timer = Timer()
+    var eighthBeatTimer: Timer = Timer()
     var beatIndex: Int = 0
+    var halfBeatIndex: Int = 0
+    var quaterBeatIndex: Int = 0
+    var eighthBeatIndex: Int = 0
     var beats: [Int] = [1, 2, 3, 4]
     var bpmValue: Int = 120
 
     func runGlobalCLock() {
         beatIndex = 0
-        timer = Timer.scheduledTimer(timeInterval: TimeInterval(1/(Double(bpmValue)/60.0)),
-                                     target: self, selector: #selector(self.globalClockTest),
-                                     userInfo: nil, repeats: true)
+        eighthBeatTimer = Timer.scheduledTimer(withTimeInterval: TimeInterval(calculateEighthBeatPerSecondValue()),
+                                               repeats: true,
+                                               block: {_ in self.eighthBeatNotification()})
     }
 
-    func changeMetronomeSpeed(toBPM: Float) {
+    func calculateBeatPerSecondValue() -> Double {
+        return 1/(Double(bpmValue)/60.0)
+    }
+
+    func calculateEighthBeatPerSecondValue() -> Double {
+        return (1/(Double(bpmValue)/60.0))/8
+    }
+
+    func changeBpmValue(toBPM: Float) {
         timer.invalidate()
+        halfBeatTimer.invalidate()
+        quaterBeatTimer.invalidate()
+        eighthBeatTimer.invalidate()
         bpmValue = Int(toBPM)
         runGlobalCLock()
     }
 
-    @IBAction func globalClockTest() {
+    func globalClockNotification() {
+        print("Full beat in full")
         if beatIndex == 0 {
             NotificationCenter.default.post(name: Notification.Name(rawValue: "globalClockBar"), object: nil)
         }
@@ -36,8 +54,29 @@ class GlobalClock {
         NotificationCenter.default.post(name: name, object: nil)
         if beatIndex == 3 {
             beatIndex = 0
+            halfBeatIndex = 0
+            quaterBeatIndex = 0
+            eighthBeatIndex = 0
         } else {
             beatIndex += 1
+        }
+    }
+
+    func eighthBeatNotification() {
+        switch eighthBeatIndex {
+        case 0, 7, 15, 23:
+            print("Full beat by eights")
+            globalClockNotification()
+        default:
+            break
+        }
+        print("eighth note: " + String(eighthBeatIndex))
+        let notificationName = Notification.Name(rawValue: "eighthNote")
+        NotificationCenter.default.post(name: notificationName, object: nil)
+        if eighthBeatIndex < 31 {
+            eighthBeatIndex += 1
+        } else {
+            eighthBeatIndex = 0
         }
     }
 
