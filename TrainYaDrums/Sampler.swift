b63b596057fd6d26d1f2446d743134bfb8b5a118
tomnvt <tnovotny@simpleway.cz>
2018-09-02 15:57:35 +0200

    Add Sampler class to subclassing individual samplers

---
+++ TrainYaDrums/Sampler.swift
@@ -0,0 +1,19 @@
+//
+//  Sampler.swift
+//  DrumTrainer
+//
+//  Created by NVT on 01.09.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class Sampler: AKAppleSampler {
+
+    let sampleLibrary = SampleLibrary()
+    let sampleLoader = SampleLoader()
+
+    func loadSamples() {}
+
+}
