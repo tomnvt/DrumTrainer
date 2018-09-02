a653ab62a76a2a6625f6b11bc2d7e9dbdf32153d
tomnvt <tnovotny@simpleway.cz>
2018-09-03 00:05:46 +0200

    Add SampleLoader and SampleLibrary classes

---
+++ TrainYaDrums/Audio/SampleLibrary.swift
@@ -0,0 +1,37 @@
+//
+//  SampleLibrary.swift
+//  DrumTrainer
+//
+//  Created by NVT on 31.08.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class SampleLibrary {
+
+    public let samples: [Sample] = [Sample("Drums/bass_drum", "_C1.wav"),
+                                    Sample("Drums/snare", "_D1.wav"),
+                                    Sample("Drums/closed_hi_hat", "_F#1.wav"),
+                                    Sample("Drums/open_hi_hat", "_A#1.wav"),
+                                    Sample("Drums/lo_tom", "_F1.wav"),
+                                    Sample("Drums/mid_tom", "_B1.wav"),
+                                    Sample("Drums/hi_tom", "_D2.wav"),
+                                    Sample("Drums/clap", "_D#1.wav"),
+                                    Sample("Drums/metronomeSound1", "_C3.wav"),
+                                    Sample("Drums/metronomeSound2", "_C#3.wav")]
+
+    public let metronomeSamples: [Sample] = [Sample("Drums/metronomeSound1", "_C3.wav"),
+                                             Sample("Drums/metronomeSound2", "_C#3.wav")]
+
+    public let drumSamples: [Sample] = [Sample("Drums/bass_drum", "_C1.wav"),
+                                        Sample("Drums/snare", "_D1.wav"),
+                                        Sample("Drums/closed_hi_hat", "_F#1.wav"),
+                                        Sample("Drums/open_hi_hat", "_A#1.wav"),
+                                        Sample("Drums/lo_tom", "_F1.wav"),
+                                        Sample("Drums/mid_tom", "_B1.wav"),
+                                        Sample("Drums/hi_tom", "_D2.wav"),
+                                        Sample("Drums/clap", "_D#1.wav")]
+
+}
---
+++ TrainYaDrums/Audio/SampleLoader.swift
@@ -0,0 +1,29 @@
+//
+//  Sample.swift
+//  DrumTrainer
+//
+//  Created by NVT on 24.08.18.
+//  Copyright © 2018 NVT. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class SampleLoader {
+
+    private let sampleLibrary = SampleLibrary()
+
+    public func loadSamples(sampler: AKAppleSampler, samples: [Sample]) {
+        var audioFiles = [AKAudioFile]()
+        for sample in samples {
+            guard let audioFile = sample.audioFile else { continue }
+            audioFiles.append(audioFile)
+        }
+        do {
+            try sampler.loadAudioFiles(audioFiles)
+        } catch {
+            print("Error while loading drums")
+        }
+    }
+
+}
